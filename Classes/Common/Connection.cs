b2a681e36085bc2bfd5ba681f982dbcb076ebc50
nightSanches <galesavich@gmail.com>
2024-06-23 19:08:41 +0500

    Реализованы классы подключения к БД

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CourseProject.Classes.Common
+{
+    public class Connection
+    {
+        private static readonly string config = "server=SANCHES\\SQLEXPRESS;Trusted_Connection=No;DataBase=Airport;User=sa;pwd=sa";
+
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
